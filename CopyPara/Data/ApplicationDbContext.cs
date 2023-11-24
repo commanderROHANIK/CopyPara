883d490767087c96d55ca76764bd2d9ecf364d96
Tamás Kis <kis6724@gmail.com>
2023-11-24 23:03:55 +0100

    Set entities in DbContext

--- CopyPara/Data/ApplicationDbContext.cs
+++ CopyPara/Data/ApplicationDbContext.cs
@@ -1,4 +1,9 @@
+using CopyPara.Domain.Cancer;
+using CopyPara.Domain.Doctors;
+using CopyPara.Domain.Machines;
+using CopyPara.Domain.Occasions;
 using CopyPara.Domain.Patients;
+using CopyPara.Domain.Treatments;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +12,18 @@ namespace CopyPara.Data;
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Patient> Patients => Set<Patient>();
+
+    public DbSet<Doctor> Doctors => Set<Doctor>();
+
+    public DbSet<Machine> Machines => Set<Machine>();
+
+    public DbSet<Occasion> Occasions => Set<Occasion>();
+
+    public DbSet<Treatment> Treatments => Set<Treatment>();
+
+    public DbSet<MachineType> MachineTypes => Set<MachineType>();
+
+    public DbSet<Cancer> Cancers => Set<Cancer>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
