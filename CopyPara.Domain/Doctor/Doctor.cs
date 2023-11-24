107a138a295d02c8b7a0b3b6ac345b648f88f4a1
Tamás Kis <kis6724@gmail.com>
2023-11-24 22:21:40 +0100

    Doctor type

---
+++ CopyPara.Domain/Doctor/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace CopyPara.Domain;
+
+public class Doctor
+{
+    public ulong Id { get; set; }
+
+    public string ApplicationUserId { get; set; }
+
+    public ICollection<Treatment> Treatments { get; set; } = [];
+}
