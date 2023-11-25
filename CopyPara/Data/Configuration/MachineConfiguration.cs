646c068b026f8d74b0be80f3db089f8bbbe557b7
Tamás Kis <kis6724@gmail.com>
2023-11-25 08:54:37 +0100

    Add data

--- CopyPara/Data/Configuration/MachineConfiguration.cs
+++ CopyPara/Data/Configuration/MachineConfiguration.cs
@@ -13,5 +13,43 @@ internal sealed class MachineConfiguration : IEntityTypeConfiguration<Machine>
         builder.HasKey(x => x.Id);
 
         builder.HasOne(x => x.Utilization).WithOne(x => x.Machine).HasForeignKey<Utilization>(e => e.MachineId);
+
+        // FillData(builder);
+    }
+
+    private void FillData(EntityTypeBuilder<Machine> builder)
+    {
+        builder.HasData([
+            new()
+            {
+                Id = 5,
+                Name = "Unique - Clinac",
+                MachineTypeId = 1,
+            },
+            new()
+            {
+                Id = 3,
+                Name = "VitalBeam 1",
+                MachineTypeId = 2,
+            },
+            new()
+            {
+                Id = 4,
+                Name = "VitalBeam 2",
+                MachineTypeId = 2,
+            },
+            new()
+            {
+                Id = 1,
+                Name = "TrueBeam 1",
+                MachineTypeId = 3,
+            },
+            new()
+            {
+                Id = 2,
+                Name = "TrueBeam 2",
+                MachineTypeId = 3,
+            },
+        ]);
     }
 }
