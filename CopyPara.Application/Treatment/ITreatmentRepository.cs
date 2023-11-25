b4f0e6504fe317957bb9afdf6185b6464fb48cb5
Szandi <aleksandra.apro97@gmail.com>
2023-11-25 22:14:03 +0100

    Create Treatment list page

--- CopyPara.Application/Treatment/ITreatmentRepository.cs
+++ CopyPara.Application/Treatment/ITreatmentRepository.cs
@@ -8,5 +8,6 @@ namespace CopyPara.Application.Treatment
         Task<GetCancer.Cancer[]> GetCancersForSelectAsync(CancellationToken cancellationToken = default);
         ValueTask<Cancer?> GetCancerAsync(ulong cancerId, CancellationToken cancellationToken = default);
         ValueTask<Domain.Treatments.Treatment?> GetTreatmentAsync(ulong treatmentId, CancellationToken cancellationToken = default);
+        Task<Domain.Treatments.Treatment[]> GetAllTreatment(CancellationToken cancellationToken = default);
     }
 }
