50fc1f9fb89eddc420f941dd1a04a2d9ec861c79
Balazs Kerekes <balazs.kerekes.ext@evosoft.com>
2023-11-25 23:01:25 +0100

    Add migration for adding name to doctor

---
+++ CopyPara/Migrations/20231125214035_AddNameForDoctor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CopyPara.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameForDoctor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Doctors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Doctors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Doctors");
+        }
+    }
+}
