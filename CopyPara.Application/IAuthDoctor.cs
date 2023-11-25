362616c9739859f92563c5d6446d0db6e0bfccf4
Tamás Kis <kis6724@gmail.com>
2023-11-25 04:57:40 +0100

    use IAuthDoctor

---
+++ CopyPara.Application/IAuthDoctor.cs
@@ -0,0 +1,8 @@
+﻿using CopyPara.Domain.Doctors;
+
+namespace CopyPara.Application;
+
+public interface IAuthDoctor
+{
+    Task<Doctor?> GetAuthenticatedDoctorAsync(CancellationToken cancellationToken = default);
+}
---
+++ CopyPara/Services/AuthenticatedDoctor.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using CopyPara.Application;
+using CopyPara.Data;
+using CopyPara.Domain.Doctors;
+using Microsoft.EntityFrameworkCore;
+
+namespace CopyPara;
+
+public sealed class AuthenticatedDoctor : IAuthDoctor
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ApplicationDbContext _context;
+
+    public AuthenticatedDoctor(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+    {
+        _context = context;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<Doctor?> GetAuthenticatedDoctorAsync(CancellationToken cancellationToken = default)
+    {
+        var id = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(id))
+        {
+            return null;
+        }
+
+        Console.WriteLine(id);
+
+        var doc = await _context.Doctors.FirstOrDefaultAsync(x => x.ApplicationUserId == id, cancellationToken);
+
+        return doc;
+    }
+}
